d30a941d631dc8f88b1e4e4754cd4ff57ccfec43
Pragatiblockchain <pragati.pandey@codezeros.com>
2024-03-12 12:18:48 +0530

    changes in contract

--- contracts/Compound_Nft.sol
+++ contracts/Compound_Nft.sol
@@ -1,50 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
+//import library
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/Compound.sol";
 
-contract MyToken is ERC721 {
-    IERC20 public token;
-    CErc20 public cToken;
+//Contract
+contract MyNFT is ERC721 {
+    using SafeMath for uint256;
+    address public admin;
+    IERC20 private token;
+    CErc20 private cToken;
     uint256 private _nextTokenId;
-    address owner;
 
-    event Log(string message, uint256 val);
-
-    constructor(address _token, address _cToken) ERC721("NFTSupply", "NFT") {
+    // token & cToken address of underlying asset
+    constructor(address _token, address _cToken) ERC721("MyNft", "NFT") {
         token = IERC20(_token);
         cToken = CErc20(_cToken);
+        admin = msg.sender;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function safeMint(uint256 _amount) public {
-        require(_amount == 100, "Please add 100 Dai Token");
+    // supply 100Dai to mint Nft
+    function mint(uint256 _amount) external {
+        require(_amount >= 100, "minimum amount is 100 Dai");
         token.transferFrom(msg.sender, address(this), _amount);
         token.approve(address(cToken), _amount);
-
-        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit Log("Exchange Rate (scaled up): ", exchangeRateMantissa);
-
-        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit Log("Supply Rate: (scaled up)", supplyRateMantissa);
-
-        require(cToken.mint(_amount) == 0, "mint failed");
+        require(cToken.mint(_amount) == 0, "Failed to mint cToken");
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
-
-    function burnToken(uint256 tokenId, uint256 _cTokenAmount) public {
-        require(cToken.redeem(_cTokenAmount) == 0, "redeem failed");
+    // burn nft to redeem 100Dai
+    function burn(uint256 tokenId) external {
+        uint256 cTokenBalance = cToken.balanceOf(address(this));
+        require(cToken.redeem(cTokenBalance) == 0, "Failed to redeem cToken");
+        uint256 tokenBalance = token.balanceOf(address(this));
+        token.transfer(msg.sender, tokenBalance);
         _burn(tokenId);
     }
 
-    function withdrawToken(uint256 _amount) external onlyOwner {
-        token.transfer(msg.sender, _amount);
+    // only admin can withdraw 
+    function withdraw(uint256 cTokenBalance) external {
+        require(msg.sender == admin, "only admin can withdraw");
+        uint256 interest = calculateInterest(cTokenBalance);
+        uint256 adminFee = interest.mul(10).div(100);
+        token.transfer(admin, adminFee);
+    }
+    
+    // internal function to calculate interest
+    function calculateInterest(
+        uint256 cTokenBalance
+    ) internal returns (uint256) {
+        uint256 exchangeRate = cToken.exchangeRateCurrent();
+        uint256 tokenBalance = cTokenBalance.mul(exchangeRate);
+        return tokenBalance.sub(100);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/96e3d3df219e45da8915a194b6a94ceb",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_APIKEY
+  },
+  sourcify: {
+    enabled: false
+  },
+  solidity: "0.8.20"
 };
